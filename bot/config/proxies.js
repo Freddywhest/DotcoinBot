ef1ea2090804cc22c3035bc347df43cab9ffeb91
Color-maN <dorokhovsasha666@gmail.com>
2024-12-08 05:59:05 +0500

    DTC Mining Update

--- bot/config/proxies.js
+++ bot/config/proxies.js
@@ -1,28 +1,96 @@
-//Currently only http and https proxies are supported.
-//HTTP proxies are not supported as they required a completely different connection type.
+//Прокси раздаются хуй пойми как. Только прокси http(s)
 
 const proxies = [
-  // EXAMPLE:
   {
-    ip: "123.456.789.123", // Proxy host (IP or hostname)
-    port: 1234, // Proxy port
-    protocol: "http", // Proxy protocol [http/https].
-    username: "username", // If use Socks with auth then you need to provide a username.
-    password: "password", // If use Socks with auth then you need to provide a password.
+    ip: "185.125.216.124", // Darina
+    port: 21034,
+    protocol: "http",
+    username: "fr7xGV1sqS",
+    password: "AmFET51ypw",
   },
   {
-    ip: "123.456.789.123", // Proxy host (IP or hostname)
-    port: 1234, // Proxy port
-    protocol: "http", // Proxy protocol [http/https].
-    username: "username", // If use Socks with auth then you need to provide a username.
-    password: "password", // If use Socks with auth then you need to provide a password.
+    ip: "45.143.202.154", // Darina2
+    port: 11986,
+    protocol: "http",
+    username: "wqouKPB0hJ",
+    password: "wRrVW89Plo",
   },
   {
-    ip: "123.456.789.123", // Proxy host (IP or hostname)
-    port: 1234, // Proxy port
-    protocol: "http", // Proxy protocol [http/https].
-    username: "username", // If use Socks with auth then you need to provide a username.
-    password: "password", // If use Socks with auth then you need to provide a password.
+    ip: "185.125.216.19", // DmitDorok
+	port: 23982,
+    protocol: "http",
+    username: "aSem7LnXgD",
+    password: "TQNuqiWr7H",
+  },
+  {
+    ip: "213.166.74.55", // Dmitriy
+	port: 9185,
+    protocol: "http",
+    username: "DAwKBc",
+    password: "q6K9An",
+  },
+  {
+    ip: "185.5.249.209", // Durian
+	port: 11599,
+    protocol: "http",
+    username: "tEbhMUpY8R",
+    password: "sxIrykefuV",
+  },
+  {
+    ip: "185.128.214.191", // Kirill
+    port: 13558,
+    protocol: "http",
+    username: "Tud7Yf9251",
+    password: "3tsn9eIQLl",
+  },
+  {
+    ip: "194.67.204.213", // KirillMain
+    port: 15644,
+    protocol: "http",
+    username: "Ll6aTHPsx5",
+    password: "YZdnmaz8PV",
+  },
+  {
+    ip: "185.66.15.235", // Kolya
+    port: 12266,
+    protocol: "http",
+    username: "uKOcqY7p3x",
+    password: "jPqVSkbnc8",
+  },
+  {
+    ip: "185.87.51.238", // Lena
+    port: 21375,
+    protocol: "http",
+    username: "0NZ4g8d3o2",
+    password: "1vnH4rDUt5",
+  },
+  {
+    ip: "92.63.202.224", // NikShih
+    port: 12819,
+    protocol: "http",
+    username: "t8HrB1qisT",
+    password: "KgzpFJCVjd",
+  },
+  {
+    ip: "185.5.251.33", // Petr
+    port: 13148,
+    protocol: "http",
+    username: "5HWQZF9AbS",
+    password: "vwrxkJTh7m",
+  },
+  {
+    ip: "185.5.250.37", // Sasha
+    port: 25403,
+    protocol: "http",
+    username: "nPGR9agj34",
+    password: "BR4emGbjcx",
+  },
+  {
+    ip: "185.5.251.189", // Sergey
+    port: 16994,
+    protocol: "http",
+    username: "fqyEskF7Ne",
+    password: "rBvAgGmlkL",
   },
 ];
 
